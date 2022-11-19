4bb5ec4ab45f55521b336aca7a4bdbb4ecd4406b
MariaAngelicaSierraQuinteroComfenalco <angelicasierr904@gmail.com>
2022-11-19 13:08:28 -0500

    Solucion 3

--- forma_Incluir_Javascript_02_19_11_2022/main.js
+++
@@ -1,8 +0,0 @@
-addEventListener(`DOMContentLoaded`, (e)=>{
-
-    console.log("Conexión con el archivo JavaScript Exitoso");
-    
-    document.querySelector("body").innerHTML = "Ver la consola del navegador o oprima la tecla F12 para ver los resultados";
-
-
-})
